87cc12985cc6f88d7cfe61dff2cee9d7b5b3a7d4
PatriceJiang <397136899@qq.com>
2022-07-21 14:37:45 +0800

    add mac & ios

---
+++ NewProject/native/plugins/aes/mac/aes_glue-config.cmake
@@ -0,0 +1,9 @@
+set(_AES_GLUE_DIR ${CMAKE_CURRENT_LIST_DIR})
+
+
+add_library(aes STATIC IMPORTED GLOBAL)
+set_target_properties(aes PROPERTIES
+    IMPORTED_LOCATION ${_AES_GLUE_DIR}/lib/libAES.a
+)
+
+include(${_AES_GLUE_DIR}/../src/CMakeLists.txt)
