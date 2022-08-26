b2b935cada79619e8486c732ad35242b4ea00f8d
Patrice Jiang <397136899@qq.com>
2022-08-26 09:38:26 +0800

    add amc

---
+++ NewProject/build/mac/data/assets/l10n/index.js
@@ -0,0 +1,20 @@
+System.register("chunks:///_virtual/l10n",[],(function(){"use strict";return{execute:function(){}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/l10n', 'chunks:///_virtual/l10n'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
---
+++ NewProject/build/mac/data/src/cocos-js/wait-for-ammo-instantiation.js
@@ -0,0 +1,1 @@
+System.register(["./instantiated-41aac2a4.js"],(function(t){"use strict";return{setters:[function(e){t("default",e.h8)}],execute:function(){}}}));
