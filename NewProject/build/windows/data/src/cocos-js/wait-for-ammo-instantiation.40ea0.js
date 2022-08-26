3f17cf10c3cd51b54fdfb14266f2677ab2fd2eee
Patrice Jiang <397136899@qq.com>
2022-08-26 09:54:11 +0800

    add windows

---
+++ NewProject/build/windows/data/src/cocos-js/wait-for-ammo-instantiation.40ea0.js
@@ -0,0 +1,13 @@
+System.register(['./instantiated-21cd46c3.js'], function (exports) {
+	'use strict';
+	return {
+		setters: [function (module) {
+			exports('default', module.hd);
+		}],
+		execute: function () {
+
+
+
+		}
+	};
+});
